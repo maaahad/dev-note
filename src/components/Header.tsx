a6974467364136a90427d511a70dd5832543ca19
Muhammed Ahad <maaahad@gmail.com>
2026-09-05 20:54:25 +0200

    navigation updated and git link added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,27 +6,21 @@ export function Header() {
     <header className="site-header">
       <div className="site-header-inner">
         <Link href="/" className="site-brand" aria-label="Home">
-          <span className="site-brand-mark">&lt;MA /&gt;</span>
+          <span className="site-brand-mark">M.A.</span>
 
           <span className="site-brand-path">~/dev-note</span>
         </Link>
 
         <nav className="site-nav" aria-label="Primary navigation">
-          <Link href="/projects">
-            <span className="nav-index">01</span>
-            projects
-          </Link>
+          <Link href="/projects">[ projects ]</Link>
 
-          <Link href="/blog">
-            <span className="nav-index">02</span>
-            blog
-          </Link>
+          <Link href="/blog">[ blog ]</Link>
 
-          <Link href="/about">
-            <span className="nav-index">03</span>
-            about
-          </Link>
+          <Link href="/about">[ about ]</Link>
 
+          <Link href="https://github.com/maaahad" target="_blank">
+            [ git ]
+          </Link>
           <ThemeToggle />
         </nav>
       </div>
